d01d4aa0ea482d6836b96ba48fbdcdb42a2cac1e
algol007 <adhyformerz@gmail.com>
2019-06-14 14:57:08 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var firstModal = document.querySelector('.first-modal');
+var secondModal = document.querySelector('.second-modal');
+
+function gotIt () {
+    firstModal.style.display = 'none';
+}
+
+function closeButton () {
+    secondModal.style.display = 'none';
+    // console.log('ok');
+}
