6b3ea5a7d947a4c76ee235a32f6914baf8eea8da
algol007 <adhyformerz@gmail.com>
2019-06-14 19:57:09 +0800

    styling fixed

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var firstModal = document.querySelector('.first-modal');
 var secondModal = document.querySelector('.second-modal');
+var imageLogo = document.querySelector('.image-logo');
 
 function gotIt () {
     firstModal.style.display = 'none';
+    imageLogo.style.margin = '0'; 
 }
 
 function closeButton () {
